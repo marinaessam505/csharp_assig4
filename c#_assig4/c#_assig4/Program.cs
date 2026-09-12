8ac351301f841b1c9cc76b8e8605c73074a39749
MARINA ESSAM <marinaessam505@gmail.com>
2026-09-12 11:28:32 +0300

    q9

--- c#_assig4/c#_assig4/Program.cs
+++ c#_assig4/c#_assig4/Program.cs
@@ -134,29 +134,47 @@ namespace c__assig4
             // true and sets price to 25.5 if title is "Clean Code", otherwise returns false and
             // sets price to 0. Call it and print the price if found.
 
-            string title = "Clean Code";
+            //string title = "Clean Code";
 
-            if (TryGetPrice(title, out double price))
-            {
-                Console.WriteLine(price);
-            }
+            //if (TryGetPrice(title, out double price))
+            //{
+            //    Console.WriteLine(price);
+            //}
 
-            static bool TryGetPrice(string title, out double price)
-            {
-                if (title == "Clean Code")
-                {
-                    price = 25.5;
-                    return true;
-                }
-                else
-                {
-                    price = 0;
-                    return false;
-                }
-            }
+            //static bool TryGetPrice(string title, out double price)
+            //{
+            //    if (title == "Clean Code")
+            //    {
+            //        price = 25.5;
+            //        return true;
+            //    }
+            //    else
+            //    {
+            //        price = 0;
+            //        return false;
+            //    }
+            //}
 
             #endregion
 
+
+
+
+            #region Question 9
+
+            // 2-Write a method PrintBookInfo(string title, int pages = 300) where pages is
+            // optional. Call it once with only a title, and once passing both a title and pages.
+
+            PrintBookInfo("Clean Code");
+
+            PrintBookInfo("cloud devops", 500);
+
+            static void PrintBookInfo(string title, int pages = 300)
+            {
+                Console.WriteLine("Book title: " + title + ", Pages: " + pages);
+            }
+
+            #endregion
 
 
 
