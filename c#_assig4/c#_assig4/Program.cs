a276601775b87b4525385293602081840dd5d24c
MARINA ESSAM <marinaessam505@gmail.com>
2026-09-12 10:15:19 +0300

    q2

--- c#_assig4/c#_assig4/Program.cs
+++ c#_assig4/c#_assig4/Program.cs
@@ -11,13 +11,23 @@
 
             // 1-Create a one-dimensional array double[] prices with the values 25.5, 40.0, 33.75. Print the second price (index 1).
 
-            double[] prices = { 25.5, 40.0, 33.75 };
+            //double[] prices = { 25.5, 40.0, 33.75 };
 
-            Console.WriteLine(prices[1]);
+            //Console.WriteLine(prices[1]);
 
             #endregion
 
 
+            #region Question Two
+
+            // 2-Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and
+            // shelf 1 has 1, 4 copies. Print the number of copies on shelf 1, slot 0.
+
+            int[,] shelfCopies =
+            { { 3, 5 }, { 1, 4 } };
+            Console.WriteLine(shelfCopies[1, 0]);
+
+            #endregion
 
 
 
