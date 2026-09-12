67b3dc1ff99aa30ba8738e3bc453e70ee0bb620d
MARINA ESSAM <marinaessam505@gmail.com>
2026-09-12 10:26:39 +0300

    q3

--- c#_assig4/c#_assig4/Program.cs
+++ c#_assig4/c#_assig4/Program.cs
@@ -23,9 +23,28 @@
             // 2-Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and
             // shelf 1 has 1, 4 copies. Print the number of copies on shelf 1, slot 0.
 
-            int[,] shelfCopies =
-            { { 3, 5 }, { 1, 4 } };
-            Console.WriteLine(shelfCopies[1, 0]);
+            //int[,] shelfCopies =
+            //{ { 3, 5 }, { 1, 4 } };
+            //Console.WriteLine(shelfCopies[1, 0]);
+
+            #endregion
+
+
+
+            #region Question Three
+
+            // 3-Write a method called PrintWelcomeMessage that takes no parameters and prints
+            // "Welcome to the Library!". Call it from Main.
+
+            static void PrintWelcomeMessage()
+            {
+                Console.WriteLine("Welcome to the Library!");
+            }
+
+            
+            
+                PrintWelcomeMessage();
+            
 
             #endregion
 
