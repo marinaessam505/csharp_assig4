cfdb4af079df1547a3e5620f33a158cdb29ef782
MARINA ESSAM <marinaessam505@gmail.com>
2026-09-12 11:33:26 +0300

    q10

--- c#_assig4/c#_assig4/Program.cs
+++ c#_assig4/c#_assig4/Program.cs
@@ -158,16 +158,28 @@ namespace c__assig4
             #endregion
 
 
-
-
             #region Question 9
 
             // 2-Write a method PrintBookInfo(string title, int pages = 300) where pages is
             // optional. Call it once with only a title, and once passing both a title and pages.
 
-            PrintBookInfo("Clean Code");
+            //PrintBookInfo("Clean Code");
 
-            PrintBookInfo("cloud devops", 500);
+            //PrintBookInfo("cloud devops", 500);
+
+            //static void PrintBookInfo(string title, int pages = 300)
+            //{
+            //    Console.WriteLine("Book title: " + title + ", Pages: " + pages);
+            //}
+
+            #endregion
+
+            #region Question 10
+
+            // 3-Using the PrintBookInfo method from the question above, call it by naming
+            // the parameters, passing pages before title.
+
+            PrintBookInfo(pages: 500, title: "cloud devops");
 
             static void PrintBookInfo(string title, int pages = 300)
             {
